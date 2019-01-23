96d4eefd65b0c099ec19c28aff75d40886f13987
unknown <afgarcia0479@misena.edu.co>
2019-01-23 00:38:48 -0500

    Add Model Sede

---
+++ database/migrations/2019_01_22_232912_create_sedes_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSedesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sede', function (Blueprint $table) {
+            $table->increments('sede_id');
+            $table->string('sede_name')->unique();
+            $table->string('sede_direccion');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sede');
+    }
+}
