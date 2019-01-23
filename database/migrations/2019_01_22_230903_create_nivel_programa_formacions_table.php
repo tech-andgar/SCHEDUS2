fc510017dfb62872619d0851ee09fa9807a2039f
unknown <afgarcia0479@misena.edu.co>
2019-01-23 00:35:36 -0500

    Add Model Nivel programa formacion

---
+++ database/migrations/2019_01_22_230903_create_nivel_programa_formacions_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNivelProgramaFormacionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('nivel_programa_formacion', function (Blueprint $table) {
+            $table->increments('nivel_programa_formacion_id');
+            $table->string('nivel_programa_formacion_name')->unique();
+            $table->string('nivel_programa_formacion_duracion');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('nivel_programa_formacion');
+    }
+}
