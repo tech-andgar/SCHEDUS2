bec233728a58313e01d87240f3bd9cae16cbf57d
unknown <afgarcia0479@misena.edu.co>
2019-01-23 00:40:35 -0500

    Add Model Trimestre

---
+++ app/Models/Trimestre.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace SCHEDUS\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Trimestre extends Model
+{
+    //
+}
---
+++ database/migrations/2019_01_22_233503_create_trimestres_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTrimestresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trimestre', function (Blueprint $table) {
+            $table->increments('trimestre_id');
+            $table->string('trimestre_name')->unique();
+            $table->date('trimestre_fecha_inicio');
+            $table->date('trimestre_fecha_fin');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trimestre');
+    }
+}
