438659c3ca29b32723066d019a1d34fdf450888a
unknown <afgarcia0479@misena.edu.co>
2019-01-23 00:42:01 -0500

    Add Model Tipo Documento

---
+++ database/migrations/2019_01_22_234737_create_tipo_documentos_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTipoDocumentosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tipo_documento', function (Blueprint $table) {
+            $table->increments('tipo_documento_id');
+            $table->string('tipo_documento_name')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tipo_documento');
+    }
+}
