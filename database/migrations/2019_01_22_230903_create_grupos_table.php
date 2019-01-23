e24969bfdfce3b8e76aacd54309af99cefaa317a
unknown <afgarcia0479@misena.edu.co>
2019-01-23 00:35:13 -0500

    Add Model Grupo

---
+++ database/migrations/2019_01_22_230903_create_grupos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGruposTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('grupo', function (Blueprint $table) {
+            $table->increments('grupo_id');
+            $table->integer('ficha_cod');
+            $table->string('grupo_num', 3);
+            $table->integer('ruta_ficha_cod');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('grupo');
+    }
+}
