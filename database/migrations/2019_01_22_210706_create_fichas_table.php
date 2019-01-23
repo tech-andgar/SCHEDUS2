d205923e0e2ad14a6877160c5f36bd3b9876b515
unknown <afgarcia0479@misena.edu.co>
2019-01-23 00:33:09 -0500

    Add Model Ficha

---
+++ database/migrations/2019_01_22_210706_create_fichas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFichasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ficha', function (Blueprint $table) {
+            $table->increments('ficha_id');
+            $table->string('ficha_num');
+            $table->string('estado_ficha_cod');
+            $table->string('programa_formacion_cod');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ficha');
+    }
+}
