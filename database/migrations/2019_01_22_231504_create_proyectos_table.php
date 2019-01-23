fbfd3cf93f2e8e02e2ffefb5337982762fec68c9
unknown <afgarcia0479@misena.edu.co>
2019-01-23 00:36:40 -0500

    Add Model Proyecto

---
+++ database/migrations/2019_01_22_231504_create_proyectos_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProyectosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('proyecto', function (Blueprint $table) {
+            $table->increments('proyecto_id');
+            $table->string('proyecto_name',191)->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('proyecto');
+    }
+}
