76c15b6e3d3d7a74bf9006d86624c46fbabb82fa
unknown <afgarcia0479@misena.edu.co>
2019-01-23 00:40:54 -0500

    Add Model Modo

---
+++ database/migrations/2019_01_22_233638_create_modos_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateModosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('modo', function (Blueprint $table) {
+            $table->increments('modo_id');
+            $table->string('modo_name')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('modo');
+    }
+}
