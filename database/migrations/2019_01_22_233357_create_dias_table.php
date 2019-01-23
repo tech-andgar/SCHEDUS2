264b8c1ff66b25506e7fbe5797e2ca4896040294
unknown <afgarcia0479@misena.edu.co>
2019-01-23 00:40:09 -0500

    Add Model Dias

---
+++ database/migrations/2019_01_22_233357_create_dias_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDiasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dias', function (Blueprint $table) {
+            $table->increments('dias_id');
+            $table->string('dias_name')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dias');
+    }
+}
