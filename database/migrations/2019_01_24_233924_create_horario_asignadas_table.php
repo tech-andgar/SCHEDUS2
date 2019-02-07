1c9225b1a964d3f124e53514acdeae88c01d2e39
Andrés García <afgarcia0479@misena.edu.co>
2019-02-07 17:45:07 -0500

    Fix migration horario asignacion

--- database/migrations/2019_01_24_233924_create_horario_asignadas_table.php
+++ database/migrations/2019_01_24_233924_create_horario_asignadas_table.php
@@ -17,13 +17,11 @@ class CreateHorarioAsignadasTable extends Migration
             $table->increments('id');
             $table->integer('trimestre_id')->unsigned()->index()->nullable();
             $table->foreign('trimestre_id')->references('id')->on('trimestre');
-            $table->integer('modo_id')->unsigned()->index()->nullable();
-            $table->foreign('modo_id')->references('id')->on('modo');
             $table->integer('ruta_ficha_id')->unsigned()->index()->nullable();
             $table->foreign('ruta_ficha_id')->references('id')->on('ruta_ficha');
             $table->string('trimestre_ficha');
-            $table->integer('resultado_aprendizaje_id')->unsigned()->index()->nullable();
-            $table->foreign('resultado_aprendizaje_id')->references('id')->on('resultado_aprendizaje');
+            $table->integer('planeacion_id')->unsigned()->index()->nullable();
+            $table->foreign('planeacion_id')->references('id')->on('planeacion');
             $table->integer('instructor_id')->unsigned()->index()->nullable();
             $table->foreign('instructor_id')->references('id')->on('cms_users');
             $table->integer('dias_id')->unsigned()->index()->nullable();
