3d83753368c71b3e62efe34ad686b64baedbbf07
Andrés García <afgarcia0479@misena.edu.co>
2019-02-13 13:43:22 -0500

    Fix style margin-left sidebar empty

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -65,7 +65,7 @@
 
 <div class="app-body">
 <!-- Contenido Principal -->
-<main class="main" style="margin-left: 0px;">
+<main class="main">
     <div class="container h-100">
         <div class="row align-items-center h-100">
             <div class="col-12 mx-auto">
@@ -104,7 +104,7 @@
 
 
     <!-- FOOTER -->
-    <footer class="app-footer" style="margin-left:0;">
+    <footer class="app-footer">
         <span><a href="#">SCHEDUS</a> &copy; 2019</span>
         <span class="ml-auto mr-2">Desarrollado por <a href="https://github.com/andgar2010">Andres Garcia</a></span>&amp;&nbsp;&nbsp;<span class="my-auto"><a href="https://github.com/johannbulls">Johann Amaya</a></span>
     </footer>
