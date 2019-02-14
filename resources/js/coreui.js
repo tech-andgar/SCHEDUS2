0a54f76c52ae3ade741c9bf505d50ee0f2b7ac73
Andrés García <afgarcia0479@misena.edu.co>
2019-02-14 12:10:28 -0500

    Refactor rescources

---
+++ resources/js/coreui.js
@@ -0,0 +1,9 @@
+
+try {
+    require('jquery');
+    require('popper.js');
+    require('@coreui/coreui');
+    require('pace');
+    require('perfect-scrollbar');
+    require('chart.js');
+} catch (e) {}
