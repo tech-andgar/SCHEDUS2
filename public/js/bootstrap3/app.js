3a9c42289305f2ec67edd093b9ec541a822c2520
Andrés García <afgarcia0479@misena.edu.co>
2019-02-14 16:31:18 -0500

    Updated minify css js

--- public/js/bootstrap3/app.js
+++ public/js/bootstrap3/app.js
@@ -81,7 +81,7 @@
 /******/
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 1);
+/******/ 	return __webpack_require__(__webpack_require__.s = 3);
 /******/ })
 /************************************************************************/
 /******/ ({
@@ -44604,7 +44604,7 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
-/***/ 1:
+/***/ 3:
 /*!**********************************************!*\
   !*** multi ./resources/js/bootstrap3/app.js ***!
   \**********************************************/
