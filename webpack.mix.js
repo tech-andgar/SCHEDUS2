a854796cf81f4c31eb5c540aa256f4c73afd8c7c
Andrés García <afgarcia0479@misena.edu.co>
2019-02-14 12:06:18 -0500

    Added Config Bootstrap 3

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,6 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css');
+
+mix.js('resources/js/bootstrap3.js', 'public/js')
+   .sass('resources/sass/bootstrap3.scss', 'public/css');
