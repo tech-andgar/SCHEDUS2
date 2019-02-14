9711066af48f581f302ff14a5fb4f90ade829dc9
Andrés García <afgarcia0479@misena.edu.co>
2019-02-14 12:06:40 -0500

    Updated files minifly

--- public/js/app.js
+++ public/js/app.js
@@ -36683,15 +36683,27 @@ if (token) {
 
 /***/ }),
 
+/***/ "./resources/sass/bootstrap3.scss":
+/*!****************************************!*\
+  !*** ./resources/sass/bootstrap3.scss ***!
+  \****************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
 /***/ 0:
-/*!*************************************************************!*\
-  !*** multi ./resources/js/app.js ./resources/sass/app.scss ***!
-  \*************************************************************/
+/*!**********************************************************************************************!*\
+  !*** multi ./resources/js/app.js ./resources/sass/app.scss ./resources/sass/bootstrap3.scss ***!
+  \**********************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
 __webpack_require__(/*! C:\wamp64\www\SCHEDUS2\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\wamp64\www\SCHEDUS2\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\wamp64\www\SCHEDUS2\resources\sass\app.scss */"./resources/sass/app.scss");
+module.exports = __webpack_require__(/*! C:\wamp64\www\SCHEDUS2\resources\sass\bootstrap3.scss */"./resources/sass/bootstrap3.scss");
 
 
 /***/ })
