a94c8dd76df985abc152e44b887f2fdbfe3904f7
Andrés García <afgarcia0479@misena.edu.co>
2019-02-14 15:04:49 -0500

    Fix path Laravel mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,8 +14,9 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css');
 
-mix.js('resources/js/bootstrap3.js', 'public/js')
-   .sass('resources/sass/bootstrap3.scss', 'public/css');
+   //Bootstrap 3
+mix.js('resources/js/bootstrap3/app.js', 'public/js/bootstrap3/')
+   .sass('resources/sass/bootstrap3/app.scss', 'public/css/bootstrap3/');
 
 mix.browserSync({
     proxy: '127.0.0.1:8000'
