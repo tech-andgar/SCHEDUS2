45cc8b34fa004bc3ea218eb50a83de0f0356a371
Andrés García <afgarcia0479@misena.edu.co>
2019-02-20 09:43:19 -0500

    Added config webpack.mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,25 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css');
+
+/*
+#################
+## Bootstrap 3 ##
+#################
+*/
+// mix.js('resources/js/bootstrap3/app.js', 'public/js/bootstrap3/')
+//    .sass('resources/sass/bootstrap3/app.scss', 'public/css/bootstrap3/');
+
+mix.browserSync({
+    //open: 'external',
+    //host: 'sitio.local',
+    proxy: '127.0.0.1:8000',
+    files: [
+        'resources/views/**/*.php',
+        'app/**/*.php',
+        'routes/**/*.php',
+        'public/js/*.js',
+        'public/css/*.css'
+    ]
+})
